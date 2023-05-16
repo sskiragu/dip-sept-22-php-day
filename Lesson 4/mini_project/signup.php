defa236998c6fddb58da675bb2ae0b2796acadb8
sskiragu <skiragu@gmail.com>
2023-05-16 11:28:47 +0300

    Database connection

--- Lesson 4/mini_project/signup.php
+++ Lesson 4/mini_project/signup.php
@@ -26,5 +26,17 @@ if (isset($_POST['signup'])) {
     echo $_POST['username'];
     echo $_POST['email'];
     echo $_POST['password'];
+
+    $database_host = "localhost";
+    $database_username = "root";
+    $database_password = "";
+    $database_name = "bank_db";
+
+    $database_connection = new mysqli($database_host, $database_username, $database_password, $database_name);
+    if($database_connection->error){
+        echo "Error connecting the database";
+    }else{
+        echo "Connection successful.";
+    }
 }
 ?>
