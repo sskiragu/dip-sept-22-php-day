13f580073a076c847d83cee69b42ffbcedfacfb0
sskiragu <skiragu@gmail.com>
2023-06-06 15:07:20 +0300

    Logout

--- Lesson 6/OOP/mini_project/bank_app/views/dashboard.php
+++ Lesson 6/OOP/mini_project/bank_app/views/dashboard.php
@@ -2,10 +2,10 @@
 <html>
 <head>
     <title>Banking Dashboard</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="../public/assets/css/styles.css">
 </head>
 <body>
-    <h1>Welcome to Your Banking Dashboard</h1>
+    <h1>Welcome to Your Banking Dashboard <span><a href="logout.php">Logout</a> <!-- Logout link or button --></span></h1>
 
     <h2>Account Summary</h2>
     <p>Account Number: <?= $accountNumber ?></p>
@@ -20,7 +20,7 @@
         </tr>
         <?php foreach ($transactions as $transaction): ?>
             <tr>
-                <td><?= $transaction['date'] ?></td>
+                <td><?= $transaction['transaction_date'] ?></td>
                 <td><?= $transaction['description'] ?></td>
                 <td><?= $transaction['amount'] ?></td>
             </tr>
